d9a81e1332488fdb684bb72582acec7337b98db6
Janne Rönkkö <janne.ronkko@iki.fi>
2020-04-10 06:41:03 +0300

    Update package classifiers

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Testing',
     ],
     keywords='unittest mock patch',
